975d17d1be10990ce4d13fa9c0b7007db5c0a215
Brahim Hadriche <brahim.hadriche@gmail.com>
2018-10-15 11:44:19 -0600

    I don't know.

---
+++ Assets/Effects/Custom/RenderWithMaterial3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(RenderWithMaterialRenderer), PostProcessEvent.AfterStack, "Custom/Render With Material 3")]
+public class RenderWithMaterial3 : RenderWithMaterial
+{
+
+    
+}
---
+++ Assets/Effects/Custom/RenderWithMaterial3Editor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.Rendering.PostProcessing;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[PostProcessEditor(typeof(RenderWithMaterial3))]
+public class RenderWithMaterial3Editor  : RenderWithMaterialEditor
+{
+    
+}
